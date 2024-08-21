b4a896cafc4e9fbbc4edcdf082742895a3e22ad5
Paul Graham <paul.graham@ftt.ai>
2024-08-21 20:39:05 +0100

    Simple upatedates for make interface pretter

--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 namespace Trxlog2Html.Extensions;
 public static class StringExtensions
 {
@@ -11,11 +13,11 @@ public static class StringExtensions
     }
     public static string GetFileName(this string value)
     {
-        return value.Substring(value.LastIndexOf('/') + 1);
+        return value.Substring(value.LastIndexOf(Path.DirectorySeparatorChar) + 1);
     }
     
     public static string ToTitle(this string value)
     {
-        return value.Replace('_', ' ').Replace('-','|');
+        return value.Substring(0,value.LastIndexOf(".")).Replace('_', ' ').Replace('-', '|');
     }
 }
